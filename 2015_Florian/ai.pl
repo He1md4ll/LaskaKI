c43de980a3a04585d44c1af2581951bb197b7502
Portikus <ahuisf@gmail.com>
2016-01-28 15:35:44 +0100

    Neue KI welche distanz zum gegner und benachbarte freunde bewertet

---
+++ 2015_Florian/ai.pl
@@ -0,0 +1,191 @@
+getDepth(Depth) :-
+        aiCalculationTime(AiTime),
+        AiTime > 280000, 
+        Depth is 6, !.
+        
+getDepth(Depth) :-
+        aiCalculationTime(AiTime),
+        AiTime > 250000, 
+        Depth is 7, !.
+        
+getDepth(Depth) :-
+        aggregate_all(count, board(_,[red|_]), R),
+        aggregate_all(count, board(_,[green|_]), G), 
+        R + G > 3,
+        Depth is 6, !.
+
+getDepth(Depth):-
+        aggregate_all(count, board(_,[]), E),
+        (                                       
+                E > 14, Depth is 6
+        ;       
+                E > 12, Depth is 6
+        ;
+                E > 8, Depth is 7
+        ;
+                Depth is 8
+        ),!.
+
+getBestTurn(Field, TargetField) :-
+        abolish(bestRating/2),
+        calculateTurn(Field, TargetField, Depth, Rating),
+        write(Field),write(TargetField),nl,
+        %debugCounter(Count),
+        %write('Additional Moves: '),write(Count),nl,
+        write('Rating: '),write(Rating),nl,
+        write('Depth: '),write(Depth),nl
+        %retract(debugCounter(Count)),
+        %assertz(debugCounter(0))
+        .
+        
+calculateTurn(Field, TargetField, _, 'Schlagzwang') :-
+        isOnlyOneTurnPossible(Field, TargetField),!.
+        
+calculateTurn(Field, TargetField, Depth, Rating) :-
+        aiColor(AiColor),
+        getDepth(Depth),
+        saveBoardToBackup,
+        abSearch(AiColor,[], Depth, Rating, -10000, 10000),
+        getBest(Field, TargetField, Rating, Depth),
+        %writeBestRating,
+        saveBackupToBoard.
+
+abSearch(Color, MoveOrder,0,Rating,_,_) :-
+        setupBoard(MoveOrder),
+        calculateRating(Rating, Color, MoveOrder),
+        asserta(bestRating(MoveOrder, Rating)), !.
+
+abSearch(Color, MoveOrder,Depth,Rating,Alpha,Beta) :-
+        setupBoard(MoveOrder),
+        writeAllPossibleDraftsForIfNeeded(Color, MoveOrder),
+        enemy(Color, EnemyColor),
+        assertz(bestRating(MoveOrder, Alpha)),
+        checkHasMovesOrJumps(MoveOrder),
+        getNewMoveOrder(MoveOrder, Draft),
+        append(MoveOrder,[Draft],NewMoveOrder),
+        coolStuff(EnemyColor, MoveOrder, Rating, NewMoveOrder, Depth, Beta).
+        
+abSearch(_, MoveOrder,_,Rating,_,_) :-
+        bestRating(MoveOrder,Rating).
+        
+deleteAlphaMoveOrderRating(_, _).       
+        
+checkHasMovesOrJumps(MoveOrder) :-
+        hasPossibleJumps(MoveOrder),!.
+        
+checkHasMovesOrJumps(MoveOrder) :-
+        hasPossibleMoves(MoveOrder),!.
+        
+checkHasMovesOrJumps(MoveOrder) :-
+        saveNewBestRating(MoveOrder, -5000).    
+        
+coolStuff(EnemyColor, MoveOrder, Rating, NewMoveOrder, Depth, Beta) :-
+        bestRating(MoveOrder,Best),
+        nextLayer(EnemyColor, NewMoveOrder, V, Depth,Best,Beta),!,
+        V > Best,
+        saveNewBestRating(MoveOrder, V),
+        V >= Beta,
+        Rating is V.
+        
+nextLayer(EnemyColor, NewMoveOrder, V, Depth,Best,Beta) :-
+        NewDepth is Depth - 1,
+        NegaBeta is Best * -1,
+        NegaAlpha is Beta * -1,
+        % rekursiver Aufruf mit neuem Board, Farbe und Suchtiefe
+        abSearch(EnemyColor, NewMoveOrder,NewDepth,ThisRating,NegaAlpha,NegaBeta),
+        % Vorzeichen vom Rating drehen
+        V is ThisRating * -1,!. 
+        
+saveNewBestRating(MoveOrder, Rating) :-
+        retract(bestRating(MoveOrder,_)),
+        assertz(bestRating(MoveOrder,Rating)), !.
+        
+getNewMoveOrder(MoveOrder, Draft) :-
+        current_predicate(possibleMove/3),
+        possibleMove(MoveOrder, Field, TargetField),
+        atom_concat(Field,TargetField, Draft).
+        
+getNewMoveOrder(MoveOrder, Draft) :-
+        current_predicate(possibleJump/4),
+        possibleJump(MoveOrder, Field, _, TargetField),
+        atom_concat(Field,TargetField, Draft).
+
+getBest(Field, TargetField, Rating, _) :-
+        NegaRating is Rating * -1,
+        findall(Draft, bestRating([Draft|[]], NegaRating),List),
+        length(List, Length),
+        Length2 is Length - 1,
+        getRandomTurn(Length2, Number, Rating),
+        nth0(Number, List, Draft),
+        %checkIfDraftIsRight(Rating, Depth -1, [Draft]),
+        translateDraft(Draft, Field, TargetField),
+        !.
+getRandomTurn(_, 0,_).
+getRandomTurn(_, 0,5000).  %Bei Sieg oder Niederlage kein Random
+getRandomTurn(_, 0,-5000). %Sondern immer den ersten Zug.
+getRandomTurn(Length2, Number,_):-
+        random_between(0, Length2, Number).
+getRandomTurn(Length2, Number,_) :-
+        getRandomTurn(0, Length2, Number).
+
+checkIfDraftIsRight(5000,_,_).  %Bei Sieg oder Niederlage keine ueberpruefung
+checkIfDraftIsRight(-5000,_,_).
+checkIfDraftIsRight(Raiting, Depth, MoveOrder)  :-
+        append(MoveOrder,NextDraft,NewMoveOrder),
+        bestRating(NewMoveOrder,Raiting),
+        NextDraft = [_],
+        NegaRaiting is Raiting * -1,
+        NewDepth is Depth -1,
+        checkIfDraftIsRight(NegaRaiting,NewDepth, NewMoveOrder).
+checkIfDraftIsRight(_, 0, _).
+
+        
+setupBoard(MoveOrder) :- 
+        saveBackupToBoard,
+        applyMoves(MoveOrder),!.
+
+applyMoves([]).
+applyMoves([Head|Tail]) :-
+        translateDraft(Head, Field, TargetField),
+        applyTurn(Field, TargetField),
+        applyMoves(Tail).               
+        
+isOnlyOneTurnPossible(Field, TargetField) :-
+        current_predicate(possibleMove/3),
+        aggregate_all(count, possibleMove([],_,_), 1),
+        possibleMove([],Field,TargetField), !.
+        
+isOnlyOneTurnPossible(Field, TargetField) :-
+        current_predicate(possibleJump/4),
+        aggregate_all(count, possibleJump([],_,_,_), 1),
+        possibleJump([],Field,_,TargetField), !.        
+        
+saveBoardToBackup :- 
+        retract(backupBoard(_,_)),
+        fail.
+                                        
+saveBoardToBackup :- 
+        board(Field,Figures),
+        asserta(backupBoard(Field,Figures)),
+        fail.
+
+saveBoardToBackup.      
+
+saveBackupToBoard :- 
+        retract(board(_,_)),
+        fail.
+                                        
+saveBackupToBoard :- 
+        backupBoard(Field,Figures),
+        asserta(board(Field,Figures)),
+        fail.
+
+saveBackupToBoard.
+
+writeBestRating :-
+        bestRating(MoveOrder, Rating),
+        checkMoveOrder(MoveOrder),
+        write('MoveOrder: '), write(MoveOrder),write(' Rating: '),write(Rating),nl,fail.
+writeBestRating.
+checkMoveOrder([e8d9, _]).
+
---
+++ 2015_Florian/facts.pl
@@ -0,0 +1,247 @@
+﻿:- dynamic
+        possibleMove/3.        
+:- dynamic
+        possibleJump/4.        
+:- dynamic
+        currentColor/1.
+:- dynamic
+        aiColor/1.
+:- dynamic
+        bestRating/2.
+:- dynamic
+                aiCalculationTime/1.
+:- dynamic
+                distanceCounter/1.      
+:- dynamic
+                debugCounter/1.
+                
+% Values for evaluation of rating                       
+:- dynamic
+                soldierValue/1. 
+:- dynamic
+                generalValue/1.
+:- dynamic
+                jailedSoldierValue/1.
+:- dynamic
+                jailedJailedSoldierValue/1.
+:- dynamic
+                moveValue/1.
+:- dynamic
+                jumpValue/1.    
+:- dynamic
+                distanceValue/1.                                                                                                                
+                
+debugCounter(0).
+
+distanceCounter(0).                                                                                                
+        
+aiCalculationTime(0).        
+
+currentColor(black).
+currentColor(white).
+
+uiNameColor(white, w).
+uiNameColor(black, s).
+
+relatedColor(white, green).
+relatedColor(black, red).
+relatedColor(red, black).
+relatedColor(green, white).
+
+opponent(white,black).
+opponent(white,red).
+opponent(black,white).
+opponent(black,green).
+opponent(green,black).
+opponent(green,red).
+opponent(red,white).
+opponent(red,green).
+
+enemy(white, black).
+enemy(black, white).
+
+degrade(green,white).
+degrade(red,black).
+degrade(X,X).     % degradiere nicht
+
+% Felder, auf denen gewöhnliche Steine befördert werden
+promote(a4,black,red).
+promote(a6,black,red).
+promote(g4,white,green).
+promote(g6,white,green).
+promote(_,X,X).    % befördere nicht
+
+% Possible moves for white (from -- to)
+move(a4,b3).
+move(a4,b5).
+move(a6,b5).
+move(a6,b7).
+move(b3,c2).
+move(b3,c4).
+move(b5,c4).
+move(b5,c6).
+move(b7,c6).
+move(b7,c8).
+move(c2,d1).
+move(c2,d3).
+move(c4,d3).
+move(c4,d5).
+move(c6,d5).
+move(c6,d7).
+move(c8,d7).
+move(c8,d9).
+move(d1,e2).
+move(d3,e2).
+move(d3,e4).
+move(d5,e4).
+move(d5,e6).
+move(d7,e6).
+move(d7,e8).
+move(d9,e8).
+move(e2,f3).
+move(e4,f3).
+move(e4,f5).
+move(e6,f5).
+move(e6,f7).
+move(e8,f7).
+move(f3,g4).
+move(f5,g4).
+move(f5,g6).
+move(f7,g6).
+
+move(Field,TargetField,white):-
+     move(Field,TargetField).
+
+move(Field,TargetField,black):-
+     move(TargetField, Field).
+     
+move(Field,TargetField,green):-
+     move(Field,TargetField, white).   
+  
+move(Field,TargetField,green):-
+     move(Field,TargetField, black). 
+ 
+move(Field,TargetField,red):-
+     move(Field,TargetField, white).   
+  
+move(Field,TargetField,red):-
+     move(Field,TargetField, black).             
+
+% Possible jumps for white (from -- over -- to)
+jump(a4,b3,c2).
+jump(a4,b5,c6).
+jump(a6,b5,c4).
+jump(a6,b7,c8).
+jump(b3,c2,d1).
+jump(b3,c4,d5).
+jump(b5,c4,d3).
+jump(b5,c6,d7).
+jump(b7,c6,d5).
+jump(b7,c8,d9).
+jump(c2,d3,e4).
+jump(c4,d3,e2).
+jump(c4,d5,e6).
+jump(c6,d5,e4).
+jump(c6,d7,e8).
+jump(c8,d7,e6).
+jump(d1,e2,f3).
+jump(d3,e4,f5).
+jump(d5,e4,f3).
+jump(d5,e6,f7).
+jump(d7,e6,f5).
+jump(d9,e8,f7).
+jump(e2,f3,g4).
+jump(e4,f5,g6).
+jump(e6,f5,g4).
+jump(e8,f7,g6).
+
+jump(Field, OverField, TargetField,white):-
+     jump(Field, OverField,TargetField).
+
+jump(Field, OverField, TargetField,black):-
+     jump(TargetField, OverField, Field).
+     
+jump(Field, OverField, TargetField,green):-
+     jump(Field, OverField, TargetField,white).
+     
+jump(Field, OverField, TargetField,green):-
+     jump(Field, OverField, TargetField,black).     
+     
+jump(Field, OverField, TargetField,red):-
+     jump(Field, OverField, TargetField,white).
+  
+jump(Field, OverField, TargetField,red):-
+     jump(Field, OverField, TargetField,black).
+
+distance(X,X,0).
+distance(Field1,Field2,Distance):-
+     sub_atom(Field1,0,1,_,Row1),
+     sub_atom(Field2,0,1,_,Row2),
+     sub_atom(Field1,1,1,_,Col1),
+     sub_atom(Field2,1,1,_,Col2),
+     getDistance(Row1,Row2,DistanceRow),
+     getDistance(Col1,Col2,DistanceCol),
+     Distance is DistanceRow + DistanceCol /2,!.
+
+getDistance(X,X,0).
+getDistance(F1,F2,Distance):-
+     distanceBetween(F1,F2,Distance),!.
+     
+getDistance(F1,F2,Distance):-
+     distanceBetween(F2,F1,Distance),!.
+
+distanceBetween(a,b,1).
+distanceBetween(a,c,2).
+distanceBetween(a,d,3).
+distanceBetween(a,e,4).
+distanceBetween(a,f,5).
+distanceBetween(a,g,6).
+
+distanceBetween(b,c,1).
+distanceBetween(b,d,2).
+distanceBetween(b,e,3).
+distanceBetween(b,f,4).
+distanceBetween(b,g,5).
+
+distanceBetween(c,d,1).
+distanceBetween(c,e,2).
+distanceBetween(c,f,3).
+distanceBetween(c,g,4).
+
+distanceBetween(d,e,1).
+distanceBetween(d,f,2).
+distanceBetween(d,g,3).
+
+distanceBetween(e,f,1).
+distanceBetween(e,g,2).
+
+distanceBetween(f,g,1).
+
+distanceBetween(A,B,Distance):-
+ atomTonumber(A,AN),
+ atomTonumber(B,BN),
+ AN < BN,
+ Distance = BN-AN  ,!.
+
+distanceBetween(A,B,Distance):-
+ atomTonumber(A,AN),
+ atomTonumber(B,BN),
+ AN > BN,
+ Distance = AN-BN  ,!.
+ 
+ 
+atomTonumber('1',1).
+atomTonumber('2',2).
+atomTonumber('3',3).
+atomTonumber('4',4).
+atomTonumber('5',5).
+atomTonumber('6',6).
+atomTonumber('7',7).
+atomTonumber('8',8).
+atomTonumber('9',9).
+
+ 
+
+
+
+
